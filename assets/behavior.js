8bb2d62312e2ec8350b12e83ecf0c6c625767c4c
aureooms <aurelien.ooms@gmail.com>
2015-07-22 21:20:41 +0000

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -380,6 +380,26 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           },
           "depth": 2,
           "outline": []
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-d-ary-heap/js/src/001-core/sorted.js",
+            "projectPath": "js/src/001-core/sorted.js",
+            "targetPath": "001-core/sorted",
+            "pageTitle": "001-core/sorted",
+            "title": "sorted"
+          },
+          "depth": 2,
+          "outline": []
         }
       ]
     }, {
